089789f654d74c0663873da4af8b219002ea89f8
nurlanalieva <alievanurlana97@mail.ru>
2022-04-14 22:36:43 +0400

    cart reducer added

---
+++ store/actions/product/productActionTypes.js
@@ -0,0 +1,5 @@
+export const GET_PRODUCTS = 'GET_PRODUCTS';
+export const GET_PRODUCT = 'GET_PRODUCT';
+export const EDIT_PRODUCT = 'EDIT_PRODUCT';
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
+import cartReducer from "./cartReducer/cartReducer";
 
 export default combineReducers({
   auth: authReducer,
+  cart: cartReducer,
 });
